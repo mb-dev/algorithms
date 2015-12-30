a7eba9fbe625f158c665049097cb2c3b6fe99f0f
Moshe Bergman <bergmanm@gmail.com>
2015-12-30 08:41:58 -0800

    maxSubarray problem

---
+++ README.md
@@ -0,0 +1,6 @@
+[![Build Status](https://travis-ci.org/mb-dev/algorithms.svg?branch=master)](https://travis-ci.org/mb-dev/algorithms/)
+
+# Solutions to Algorithms Questions
+
+## EPI
+[Elements of Programming Interviews: The Insiders' Guide](http://www.amazon.com/Elements-Programming-Interviews-Insiders-Guide/dp/1479274836)
---
+++ hackerrank/dp-max-subarray.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+
+// https://www.hackerrank.com/challenges/maxsubarray
+// Given array of integers, find maximum possible contiguous subarray
+
+function maxSubarray(input) {
+  var sumSoFar = 0, bestSum = 0;
+  var start = 0, end = 0;
+  var bestStartIndex = -1, bestEndIndex = -1;
+  for (var i=0; i < input.length; ++i) {
+    sumSoFar = sumSoFar + input[i];
+    if (sumSoFar < 0) {
+      sumSoFar = 0;
+      start = end = i+1;
+    } else {
+      end = i;
+    }
+    if (sumSoFar > bestSum) {
+      bestStartIndex = start;
+      bestEndIndex = end;
+      bestSum = sumSoFar;
+    }
+  }
+  return [bestStartIndex, bestEndIndex];
+}
+
+function test() {
+  assert.deepEqual(maxSubarray([1, -3, 5, -2, 9, -8, 6, 4]), [2, 7])
+  assert.deepEqual(maxSubarray([1, -3, 5, -2, 9, -8, 6]), [2, 4])
+}
+
+module.exports = test;
