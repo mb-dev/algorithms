f6c16ef7a2d9e12d9bcf80cf0484b6c2423a740f
Moshe Bergman <mb-dev@users.noreply.github.com>
2017-01-23 22:51:41 -0800

    dijkstra

--- mit/pathfinding.js
+++ mit/pathfinding.js
@@ -2,6 +2,24 @@ const assert = require('assert');
 const testing = require('../testing/testing');
 const _ = require('lodash');
 
+// return the path used to reach the destination
+function get_path(source, min_weight, dest) {
+  const path = [];
+  let current = dest;
+  let total_weight = 0;
+  // if we didn't find any path, the source for current will be null
+  if (!source[current]) {
+    return null;
+  }
+  // otherwise, there's some path, continue until we reach the end
+  while(current != null) {
+    path.unshift(current);
+    total_weight += min_weight[current];
+    current = source[current];
+  }
+  return {path, weight: total_weight};
+}
+
 function relax(input, u, v, min_weight, source) {
   if (min_weight[v] > min_weight[u] + input[u][v]) {
     min_weight[v] = min_weight[u] + input[u][v];
@@ -15,37 +33,27 @@ function extract_min(vertices, min_weight) {
   return min;
 }
 
-function get_path(source, min_weight, dest) {
-  const path = [];
-  let current = dest;
-  let total_weight = 0;
-  while(current != null) {
-    path.unshift(current);
-    total_weight += min_weight[current];
-    current = source[dest];
-  }
-  return {path, weight: total_weight};
-}
-
+// find the shortest paths
 function dijkstra(input, src, dest) {
   const source = [];
   const min_weight = [];
   const remaining_vertices = [];
+  // initialize the array, min weight is infinity
   for (let i = 0; i < input.length; ++i) {
     source[i] = null;
-    min_weight[i] = Number.MAX_VALUE;
+    min_weight[i] = 10000;
     remaining_vertices.push(i);
   }
   min_weight[src] = 0;
   while(remaining_vertices.length > 0) {
     // find the min vertex. It will start with src, since it has the min-weight of 0
     const u = extract_min(remaining_vertices, min_weight);
-    _.forEach(input[u], (v, k) => {
+    // relax all edges coming out of the min
+    _.forEach(input[u], (weight, v) => {
       relax(input, u, v, min_weight, source);
     });
   }
-  console.log(source);
-  return get_path(source);
+  return get_path(source, min_weight, dest);
 }
 
 function test() {
@@ -54,7 +62,7 @@ function test() {
     {2: 1}, // node 1
     {},
   ], 0, 2));
-  assert.deepEqual({path: [0, 1, 2], weight: 2}, dijkstra([
+  assert.deepEqual(null, dijkstra([
     {1: 1}, // node 0
     {2: 1}, // node 1
     {},
